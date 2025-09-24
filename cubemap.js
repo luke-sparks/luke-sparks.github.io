65176541728cd203a0f8ceabbf39a4c142b647df
luke-sparks <sparks.luke.j@gmail.com>
2025-09-24 09:42:23 -0700

    Move colors outside of getBiome function and remove dead code

--- cubemap.js
+++ cubemap.js
@@ -52,18 +52,6 @@ const landHeightMapping = {
     20: 34              // Snow peaks
 };
 
-// Not using (for now?)
-// const binnedHeightMapping = [
-//     [1, 2, 3],       // water
-//     [4, 5],          // beach/lowlands
-//     [6, 7, 8],       // plains
-//     [9, 10, 11],     // low hills
-//     [12, 13, 14],    // higher hills
-//     [15, 16, 17],    // mountains
-//     [18, 29],        // peaks
-//     [20]             // mountaintops
-// ]
-
 // Worker pool for parallel face generation
 let workerPool = [];
 let taskCounter = 0;
@@ -78,6 +66,30 @@ function createWorkerBlobURL() {
 // This worker runs in a separate thread for true parallelization
 
 const rotationAngle = 0.85;
+
+const colors = [
+        null,
+        {r: 5, g: 15, b: 60},     // Level 1: Deep ocean
+        {r: 15, g: 35, b: 90},    // Level 2: Medium ocean
+        {r: 30, g: 60, b: 130},   // Level 3: Shallow ocean
+        {r: 220, g: 200, b: 160}, // Level 4: Beach
+        {r: 150, g: 200, b: 110}, // Level 5: Lowlands
+        {r: 120, g: 180, b: 80},  // Level 6: Low plains
+        {r: 100, g: 160, b: 70},  // Level 7: Plain
+        {r: 90, g: 150, b: 65},   // Level 8: Rolling hills
+        {r: 110, g: 140, b: 75},  // Level 9: Lower hills
+        {r: 115, g: 130, b: 75},  // Level 10: Hills
+        {r: 125, g: 130, b: 80},  // Level 11: Higher hills
+        {r: 140, g: 120, b: 80},  // Level 12: Highlands
+        {r: 145, g: 125, b: 85},  // Level 13: High highlands
+        {r: 150, g: 130, b: 90},  // Level 14: Lower mountains
+        {r: 160, g: 140, b: 100}, // Level 15: Mountains
+        {r: 170, g: 150, b: 120}, // Level 16: High mountains
+        {r: 180, g: 170, b: 150}, // Level 17: Alpine
+        {r: 200, g: 200, b: 210}, // Level 18: High peaks
+        {r: 220, g: 220, b: 230}, // Level 19: Snow line
+        {r: 245, g: 245, b: 255}  // Level 20: Snow peaks
+    ];
 
 class Noise {
     constructor(seed) {
@@ -181,35 +193,6 @@ function getCubeCoords(face, u, v) {
 }
 
 function getBiome(heightLevel, temperature, moisture) {
-    // Exactly 20 colors based on height levels 1-20
-    // Ocean depths unchanged: Level 1 (deep), Level 2 (medium), Level 3 (shallow)
-    // Land levels 4-20 with additional greens and greenish browns
-    
-    const colors = [
-        null,
-        {r: 5, g: 15, b: 60},     // Level 1: Deep ocean
-        {r: 15, g: 35, b: 90},    // Level 2: Medium ocean
-        {r: 30, g: 60, b: 130},   // Level 3: Shallow ocean
-        {r: 220, g: 200, b: 160}, // Level 4: Beach
-        {r: 150, g: 200, b: 110}, // Level 5: Lowlands
-        {r: 120, g: 180, b: 80},  // Level 6: Low plains
-        {r: 100, g: 160, b: 70},  // Level 7: Plain
-        {r: 90, g: 150, b: 65},   // Level 8: Rolling hills
-        {r: 110, g: 140, b: 75},  // Level 9: Lower hills
-        {r: 115, g: 130, b: 75},  // Level 10: Hills
-        {r: 125, g: 130, b: 80},  // Level 11: Higher hills
-        {r: 140, g: 120, b: 80},  // Level 12: Highlands
-        {r: 145, g: 125, b: 85},  // Level 13: High highlands
-        {r: 150, g: 130, b: 90},  // Level 14: Lower mountains
-        {r: 160, g: 140, b: 100}, // Level 15: Mountains
-        {r: 170, g: 150, b: 120}, // Level 16: High mountains
-        {r: 180, g: 170, b: 150}, // Level 17: Alpine
-        {r: 200, g: 200, b: 210}, // Level 18: High peaks
-        {r: 220, g: 220, b: 230}, // Level 19: Snow line
-        {r: 245, g: 245, b: 255}  // Level 20: Snow peaks
-    ];
-    
-    // Return the exact color for this height level
     return colors[heightLevel] || {r: 0, g: 0, b: 0};
 }
 
